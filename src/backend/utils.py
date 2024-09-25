4afe565a2f28292830b88b8671a8d9f0d3432d3b
IsaacVega5 <isaacvega361@gmail.com>
2024-09-25 14:35:42 -0300

    perf(backend): Replace function and optimice
    Se ha eliminado la función reshape_image() de utils.py y en su lugar se utiliza la función cv2.resize() de opencv para reescalar la matriz.
    Además se modifico read_envi() de hiper.py para que utilice menos funciones de pillow y nás funciones de opencv, ya que estas están mejor optimizadas.

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -22,25 +22,6 @@ def cut_points_to_array(cut_points):
             (math.floor(float(cut_points[1]['x'])), math.floor(float(cut_points[1]['y'])))]
   return points
 
-def reshape_matrix(matriz, nuevo_ancho, nuevo_alto):
-    # Obtiene las dimensiones originales
-    alto_original, ancho_original = matriz.shape
-    
-    # Crea una nueva matriz para almacenar los datos redimensionados
-    matriz_redimensionada = np.zeros((nuevo_alto, nuevo_ancho), dtype=matriz.dtype)
-    
-    # Calcula los factores de escala
-    factor_y = alto_original / nuevo_alto
-    factor_x = ancho_original / nuevo_ancho
-    
-    for i in range(nuevo_alto):
-        for j in range(nuevo_ancho):
-            # Encuentra las coordenadas correspondientes en la matriz original
-            x_original = int(j * factor_x)
-            y_original = int(i * factor_y)
-            matriz_redimensionada[i, j] = matriz[y_original, x_original]
-    
-    return matriz_redimensionada
 
 def get_new_size(image, ancho, alto):
   o_alto, o_ancho = image.shape
