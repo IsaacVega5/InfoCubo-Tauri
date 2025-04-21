e367816e5f641b5ea28b1dc9b2b4ba404f397f10
Isaac Vega <isaacvega361@gmail.com>
2025-04-21 09:48:32 -0400

    [Feat] Allow-dinamic-host-backend-build
    to_dist.py is modified to allow it to build the backend in different names depending on the host platform

--- backend/to_dist.py
+++ backend/to_dist.py
@@ -1,9 +1,25 @@
+import subprocess
 import PyInstaller.__main__
 import PyInstaller.config
 
+def get_rustc_host():
+    try:
+        # Ejecutar el comando y capturar la salida
+        output = subprocess.check_output(["rustc", "-Vv"], text=True)
+        
+        # Buscar la línea que contiene 'host:'
+        for line in output.splitlines():
+            if line.startswith("host:"):
+                # Extraer el valor después de 'host:'
+                return line.split(":", 1)[1].strip()
+        
+        return None
+    except subprocess.CalledProcessError:
+        raise RuntimeError("Error al ejecutar 'rustc -Vv'\nCodificación de error: " + str(subprocess.CalledProcessError.returncode))
+
 PyInstaller.__main__.run([
     'backend/main.py',
-    '--name', 'infocubo-backend-x86_64-pc-windows-msvc',
+    '--name', f'infocubo-backend-{get_rustc_host()}',
     '--workpath', './backend/build',
     '--distpath', './backend/dist',
     '--clean',
